e4d4ccc59c727fd82a225e3da98f7a1fd09fba8b
Unique-01 <86983696+Unique-01@users.noreply.github.com>
2022-01-29 08:11:00 +0100

    commit on 'master'

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views import generic
 # Create your views here.
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = "index.html/"
     
     # For the search bar
