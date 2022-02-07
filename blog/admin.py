172600bdc58b38d53734b030462499f6725a9e5c
Unique-01 <86983696+Unique-01@users.noreply.github.com>
2022-02-07 15:37:57 +0100

    commit on master

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Comment
 
 # Register your models here.
 @admin.register(Post)
@@ -8,3 +8,12 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status','created_on')
     prepopulated_fields = {'slug':('title',)}
     search_fields = ['title','content']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name','body','post','created_on','active',)
+    list_filter = ('active','created_on')
+    actions = ['approve_comments']
+
+    def approve_comments(self,request,queryset):
+        queryset.update(active=True)
--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,6 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS,default=0)
 
 
-
     class Meta:
         verbose_name = ("post")
         verbose_name_plural = ("posts")
@@ -29,4 +28,24 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post_detail", kwargs={"pk": self.pk})
+
+class Comment(models.Model):
+    """Model definition for Comment."""
+    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    name = models.CharField( max_length=50)
+    email = models.EmailField( max_length=254)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        """Meta definition for Comment."""
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+        ordering = ['-created_on']
+
+    def __str__(self):
+        """Unicode representation of Comment."""
+        return f"Comment {self.body} by {self.name}"
+
 
